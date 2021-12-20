7504a6e212496751e10953ecbb01818e1c10155b
Hen <hentzalah@gmail.com>
2021-12-20 17:01:43 +0200

    Json commit

--- src/main/java/com/hit/Game.java
+++ src/main/java/com/hit/Game.java
@@ -1,5 +1,7 @@
 package main.java.com.hit;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import com.google.gson.Gson;
 
@@ -10,26 +12,29 @@ public class Game {
 
     String GameStoreName;
     String AddressStore;
+    boolean Available;
 
-    public Game(String gameName, String genre, String gameCompanyDevelop,String gameStoreName, String addressStore){
+    public Game(String gameName, String genre, String gameCompanyDevelop,String gameStoreName, String addressStore, boolean available){
 
         this.GameName = gameName;
         this.Genre = genre;
         this.GameCompanyDevelop = gameCompanyDevelop;
         this.GameStoreName = gameStoreName;
         this.AddressStore = addressStore;
+        this.Available = available;
     }
 
     public Game(){}
 
     public static void main(String[] args) {
 
-        Game testGame = new Game("gta","roleplay","rockstar","PlayHard", "north 97");
+        Game testGame = new Game("gta","roleplay","rockstar","PlayHard", "north 97",true);
 
         Gson gson = new Gson();
         String json = gson.toJson(testGame);
         System.out.println(json);
 
+        File file = new File()
     }
 }
 
